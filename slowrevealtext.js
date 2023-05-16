8dc324c474b79ce4abfcb95d8cc2864967de310c
Ava-Rubin <avarubin@gmail.com>
2023-05-16 15:26:08 -0700

    Update

--- slowrevealtext.js
+++ slowrevealtext.js
@@ -8,3 +8,30 @@ function printLetterByLetter(destination, message, speed){
         }
     }, speed);
 }
+
+function printCurrDate(){
+    
+    currDate =  new Date();
+    
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September','November','December'];
+    month = months[currDate.getMonth()];
+    
+    date = currDate.getDate();
+    
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+    day = days[currDate.getDay()];
+
+    hour = currDate.getHours();
+    min = currDate.getMinutes();
+    sec = currDate.getSeconds();
+
+    printDate = "Last login: " + day + " " + month + " " + date + " " + hour + ":" + min + ":" +sec;
+
+    printLetterByLetter('msg',printDate,100);
+
+    
+}
+
+
+
+
